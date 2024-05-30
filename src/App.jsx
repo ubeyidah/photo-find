18f57a2fecde9c9b2ea310da9ad458e4bfa9958c
ubeyid oumer <ubeyidah@gmail.com>
2024-05-30 23:41:07 +0300

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import ImagesGallery from "./component/ImagesGallery";
+import React, { useEffect, useState } from "react";
+import ImageCard from "./component/ImageCard";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [images, setImages] = useState(null);
+  const [page, setPage] = useState(1);
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
+  const fetchImages = async (query) => {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${apiKey}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    if (res.ok) {
+      setImages(data.results);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages("photo");
+  }, []);
+
+  const handleBtn = () => {
+    fetchImages(searchTerm);
+  };
+  console.log(images);
   return (
     <main className="px-5 pt-52">
       <header className="flex items-center justify-center flex-col pt-10 gap-6 fixed top-0 left-0 right-0 bg-slate-100 border-b p-3 pb-6 ">
@@ -49,7 +69,7 @@ const App = () => {
               onChange={handleSearch}
             />
             <button
-              type="submit"
+              onClick={handleBtn}
               className="text-white absolute end-2.5 bottom-2.5 bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
             >
               Search
@@ -58,7 +78,10 @@ const App = () => {
         </div>
       </header>
 
-      <ImagesGallery />
+      {images &&
+        images.map((image) => (
+          <ImageCard key={image.created_at} data={image} />
+        ))}
     </main>
   );
 };
--- src/component/ImageCard.jsx
+++ src/component/ImageCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const ImagesGallery = () => {
+const ImageCard = ({ images }) => {
   return <div>ImagesGallery</div>;
 };
 
-export default ImagesGallery;
+export default ImageCard;
